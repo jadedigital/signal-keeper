222ce7d836b9f760c406eef9e82abd8f74d74b60
Aaron Greene <aaron.greene@technipfmc.com>
2019-05-21 13:56:32 -0230

    input update

--- components/account/MaintRequest.vue
+++ components/account/MaintRequest.vue
@@ -47,7 +47,7 @@
               label="Priority"
               :item-value="priority"
             ></v-select>
-            <input type="hidden" :value="priority" />
+            <v-input class="hidden" :value="priority" />
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn @click="clear">clear</v-btn>
