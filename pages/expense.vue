1cb9be9eb4b5f2a5b4899cace7cbd141afb7adb2
Aaron Greene <aaron.greene@technipfmc.com>
2019-07-05 12:50:21 -0230

    account page setup change

--- pages/expense.vue
+++ pages/expense.vue
@@ -6,7 +6,7 @@
     <div class="columns">
       <div class="one-half column centered">
         <div class="four-fifths">
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBY_vY1vr3dA_BKwyRu4cfBsUUC_eN4J2wN0lkYxGQZrvt5g/viewform?embedded=true" width="640" height="1261" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBY_vY1vr3dA_BKwyRu4cfBsUUC_eN4J2wN0lkYxGQZrvt5g/viewform?embedded=true" width="100%" height="1261" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
         </div>
       </div>
     </div>
